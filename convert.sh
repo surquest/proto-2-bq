89ebeceed23db84ea341f44cd07c029f753ba266
Michal Švarc <michal.svarc@surquest.com>
2023-07-12 19:52:27 +0200

    Adding metadata

--- convert.sh
+++ convert.sh
@@ -6,6 +6,14 @@ base_path="/tmp/schemas"
 # Get the relative file path from the first parameter
 relative_path="$1"
 
+# Check if the second parameter (add message metadata) is provided (if not set to false)
+if [ -z "$2" ]; then
+    add_message_metadata=false
+else
+    add_message_metadata="$2"
+fi
+
+
 # Get table name as string from the first parameter before the dot
 table_name=$(echo "$1" | cut -d "." -f 1)
 
@@ -41,6 +49,46 @@ protoc --bq-schema_out=$base_path --proto_path=/tmp/default --proto_path=$base_p
 
 # Remove the copied file
 rm $copied_file_path
+
+# Check if the second parameter is set to true
+if [ "$add_message_metadata" = true ]; then
+    # Define the new JSON array with Pub/Sub message metadata
+    metadata='[
+        {
+            "name": "subscription_name",
+            "type": "STRING",
+            "mode": "NULLABLE",
+            "description": "Name of a subscription"
+        },
+        {
+            "name": "message_id",
+            "type": "STRING",
+            "mode": "NULLABLE",
+            "description": "ID of a message"
+        },
+        {
+            "name": "publish_time",
+            "type": "TIMESTAMP",
+            "mode": "NULLABLE",
+            "description": "The time of publishing a message"
+        },
+        {
+            "name": "attributes",
+            "type": "JSON",
+            "mode": "NULLABLE",
+            "description": "A JSON object containing all message attributes. It also contains additional fields that are part of the Pub/Sub message including the ordering key, if present."
+        }
+    ]'
+
+    # Read the existing JSON array from the file
+    json_array=$(jq '.[]' $base_path/out/$table_name.schema)
+
+    # Append the new JSON array to the existing array
+    json_array=$(echo "$json_array" | jq --argjson metadata "$metadata" '. + $metadata')
+
+    # Write the updated JSON array back to the file
+    echo "$json_array" | jq '.' > $base_path/out/$table_name.schema
+fi
 
 # print success and path the to the generated file
 echo "---------------------------------------------------------------------------------------------------"
